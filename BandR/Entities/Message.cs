92f72af4658d7406c7f272e10598ba5cbd2a00bf
Michael <michael.arn@live.fr>
2026-07-03 21:20:05 +0200

    Add missing property

--- BandR/Entities/Message.cs
+++ BandR/Entities/Message.cs
@@ -3,6 +3,7 @@ namespace BandR.Entities;
 public class Message :BaseEntity
 {
     public string Content { get; set; } =  string.Empty;
+    public DateTime SentAt { get; set; }
     public DateTime? ReadAt { get; set; }
     public Guid ConversationId { get; set; }
     public Conversation Conversation { get; set; } = null!;
