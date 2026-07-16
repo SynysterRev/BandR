923529e25efde7e2e4717ef08d32b78077d69353
Michael <michael.arn@live.fr>
2026-07-16 20:41:05 +0200

    Continue to write tests

--- BandR.Tests/IntegrationTests/Services/ConversationServiceTests.cs
+++ BandR.Tests/IntegrationTests/Services/ConversationServiceTests.cs
@@ -1,5 +1,7 @@
 using BandR.Data;
 using BandR.DTOs.Conversation;
+using BandR.DTOs.Messages;
+using BandR.DTOs.Musicians;
 using BandR.Exceptions;
 using BandR.Services;
 using FluentAssertions;
@@ -41,22 +43,22 @@ public class ConversationServiceTests : IClassFixture<TestDatabaseFixture>, IAsy
 
     public Task DisposeAsync() => Task.CompletedTask;
 
-    private async Task<ConversationDto> CreateConversation(Guid? announcementId)
+    private async Task<(ConversationDto Conversation, MusicianDto Musician)> CreateConversation(Guid? appUserId, Guid? announcementId)
     {
-        var musician = await _fixture.CreateDefaultMusician(customUserId: Guid.NewGuid());
+        var musician = await _fixture.CreateDefaultMusician(customUserId: appUserId ?? Guid.NewGuid());
         var otherMusician = await _fixture.CreateDefaultMusician(customUserId: Guid.NewGuid());
         var createConversation = new CreateConversationDto
         (
             announcementId,
             otherMusician.Id
         );
-        return await _conversationService.CreateConversation(musician.Id, createConversation, CancellationToken.None);
+        return (await _conversationService.CreateConversation(musician.Id, createConversation, CancellationToken.None), musician);
     }
 
     [Fact]
     private async Task ShouldCreateWithoutAnnouncement()
     {
-        var conversationDto = await CreateConversation(null);
+        var (conversationDto, _) = await CreateConversation(null, null);
         
         conversationDto.Should().NotBeNull();
         conversationDto.Messages.Count.Should().Be(0);
@@ -66,7 +68,7 @@ public class ConversationServiceTests : IClassFixture<TestDatabaseFixture>, IAsy
     private async Task ShouldCreatedWithAnnouncement()
     {
         var announcement = await _fixture.CreateDefaultAnnouncement();
-        var conversationDto = await CreateConversation(announcement.Id);
+        var (conversationDto, _) = await CreateConversation(null, announcement.Id);
         
         conversationDto.Should().NotBeNull();
         conversationDto.Messages.Count.Should().Be(0);
@@ -77,7 +79,7 @@ public class ConversationServiceTests : IClassFixture<TestDatabaseFixture>, IAsy
     private async Task ShouldFailWhenAnnouncementDoesNotExist()
     {
         var announcementId = Guid.NewGuid();
-        var act = () => CreateConversation(announcementId);
+        var act = () => CreateConversation(null, announcementId);
         
         await act.Should().ThrowAsync<AnnouncementException.AnnouncementNotFoundException>().WithMessage($"Announcement with id {announcementId} not found");
     }
@@ -99,5 +101,43 @@ public class ConversationServiceTests : IClassFixture<TestDatabaseFixture>, IAsy
         
         sameConv.Should().NotBeNull();
         sameConv.Id.Should().Be(conversationDto.Id);
+    }
+    
+    [Fact]
+    private async Task ShouldSendAMessage()
+    {
+        var appUserId = Guid.NewGuid();
+        var (conversationDto, musician) = await CreateConversation(appUserId, null);
+        var message = new CreateMessageDto("This is a test message");
+        var messageDto = await _conversationService.SendMessage(musician.Id, conversationDto.Id, message, CancellationToken.None);
+        
+        messageDto.Should().NotBeNull();
+        messageDto.Content.Should().Be("This is a test message");
+        messageDto.SenderName.Should().Be(musician.Username);
+    }
+    
+    [Fact]
+    private async Task ShouldFailWhenConversationDoesNotExist()
+    {
+        var musician = await _fixture.CreateDefaultMusician(customUserId: Guid.NewGuid());
+        var message = new CreateMessageDto("This is a test message");
+        var convId = Guid.NewGuid();
+        
+        var act = () => _conversationService.SendMessage(musician.Id, convId, message, CancellationToken.None);
+        
+        await act.Should().ThrowAsync<ConversationException.ConversationNotFoundException>().WithMessage($"Conversation with id {convId} not found");
+    }
+    
+    [Fact]
+    private async Task ShouldFailWhenMusicianIsNotInConversation()
+    {
+        var appUserId = Guid.NewGuid();
+        var (conversationDto, _) = await CreateConversation(appUserId, null);
+        var message = new CreateMessageDto("This is a test message");
+        var convId = Guid.NewGuid();
+        
+        var act = () => _conversationService.SendMessage(Guid.NewGuid(), conversationDto.Id, message, CancellationToken.None);
+        
+        await act.Should().ThrowAsync<ConversationException.ConversationForbiddenException>().WithMessage($"Access to Conversation {conversationDto.Id} is forbidden");
     }
 }
